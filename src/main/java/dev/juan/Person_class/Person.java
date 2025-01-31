a32e34df5a67f646ab5736303f7a958e9d99bb59
juancastro000 <cdyutub@gmail.com>
2025-01-31 19:04:46 +0100

    fix

--- src/main/java/dev/juan/Person_class/Person.java
+++ src/main/java/dev/juan/Person_class/Person.java
@@ -4,8 +4,18 @@ public class Person {
 
     String nombre;
     String apellido;
-    int dni;
-    int año;
+    String dni;
+    int nacimiento;
+
+    public Person (String nombre, String apellido, String dni, int nacimiento){
+
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.nacimiento = nacimiento;
+
+    }
+
 
 
 }
